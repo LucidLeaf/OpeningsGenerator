a3e429e5b914b08d7d274b12958ff1ab7132bfba
Leo Schultheiss <leo.schultheiss@tum.de>
2023-07-20 15:57:30 +0200

    Implemented tree data structure

--- OpeningTree.py
+++ OpeningTree.py
@@ -1,9 +1,31 @@
-from pprint import pprint
-
 from RequestHandler import *
 
 
-def recursive_tree_search(number_of_lines: int, recursion_depth: int, list_of_played_moves: list[str] = None) -> list:
+class OpeningTree:
+    start_name = "Start"
+    move: str
+    children: list
+
+    def __init__(self, move=start_name):
+        self.move = move
+        self.children = []
+
+    def add_node(self, new_node):
+        self.children.append(new_node)
+
+    def __str__(self, level=0):
+        """String representation of the tree."""
+        if level == 0:
+            ret = self.start_name + "\n"
+        else:
+            ret = "|   " * (level - 1) + "|-- " + repr(self.move) + "\n"
+        for child in self.children:
+            ret += child.__str__(level + 1)
+        return ret
+
+
+def recursive_tree_search(number_of_lines: int, recursion_depth: int,
+                          list_of_played_moves: list[str] = None) -> OpeningTree:
     """
 
     :param number_of_lines: The number of most common move options to be considered
@@ -12,24 +34,29 @@ def recursive_tree_search(number_of_lines: int, recursion_depth: int, list_of_pl
     :return: Matrix of most common moves per line
     """
     if list_of_played_moves is None:
+        # first level of recursion, initialize move list and tree data structure
         list_of_played_moves = []
+        tree = OpeningTree()
+    else:
+        last_move = list_of_played_moves[len(list_of_played_moves) - 1]
+        tree = OpeningTree(last_move)
+
     if recursion_depth <= 0:
-        return list_of_played_moves
+        return tree
 
     # request next batch of most common moves
     json = make_request(number_of_lines=number_of_lines, played=list_of_played_moves)
     most_common_moves = get_most_common_moves(json)
 
     # recursively repeat the process for each of the next best moves
-    decision_tree = []
     for move in most_common_moves:
         new_line = list_of_played_moves.copy()
         new_line.append(move)
         resulting_move_tree = recursive_tree_search(number_of_lines, recursion_depth - 1, new_line)
-        decision_tree.append(resulting_move_tree)
-    return decision_tree
+        tree.add_node(resulting_move_tree)
+    return tree
 
 
 if __name__ == "__main__":
-    lines = recursive_tree_search(1, 3)
-    pprint(lines)
+    lines = recursive_tree_search(2, 3)
+    print(str(lines))
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is a project intended to retrieve the most common opening lines from the lichess database and then use these to automatically generate flashcards.
 
 TODO
-- create data structure to hold information about moves, opening name etc.
+- append tree data structure to hold more information
 - generate flashcards with opening tree
